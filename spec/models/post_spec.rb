72ec8d7decfbc0647c7e216fd54a5778bc9806ff
AlexDid753 <alex.didenko753@gmail.com>
2017-12-04 01:52:00 +0300

    posts tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) {FactoryBot.build :post}
+  subject {post}
+
+  it {should validate_presence_of :title}
+  it {should validate_presence_of :text}
+  it {should validate_presence_of :ip_address}
+  it {should validate_presence_of :user_id}
+
 end
